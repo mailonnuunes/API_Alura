c7c9d4d06a8f2ee95df2587f2e730dfa30d1d801
Mailon Azevedo <100615338+mailonnuunes@users.noreply.github.com>
2023-09-03 21:23:35 -0300

    Subindo arquivos

---
+++ APIAlura/Controllers/UsuarioController.cs
@@ -0,0 +1,50 @@
+﻿
+
+using APIAlura.Entidade;
+using APIAlura.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APIAlura.Controllers
+{
+    [ApiController]
+    [Route("Usuario")]
+
+   
+    public class UsuarioController : ControllerBase
+    {
+        private IUsuarioRepository _usuarioRepository;
+
+        public UsuarioController(IUsuarioRepository usuarioRepository)
+        {
+            _usuarioRepository = usuarioRepository;
+        }
+
+
+        [HttpGet("Obter-todos-os-usuarios")]
+        public IActionResult ChamarTodosUsuarios()
+        {
+            return Ok(_usuarioRepository.ObterTodosUsuarios());
+        }
+        [HttpGet("Obter-usuario-por-id/{id}")]
+        public IActionResult ChamarUsuarioID(int id)
+        {
+            return Ok(_usuarioRepository.ObterUsuarioPorId(id));
+        }
+        [HttpPut]
+        public void CadastrarUsuario([FromBody]Usuario usuario)
+        {
+            _usuarioRepository.CadastrarUsuario(usuario);
+
+        }
+        [HttpPost]
+        public void EditarUsuario([FromBody]Usuario usuario)
+        {
+            _usuarioRepository.AlterarUsuario(usuario);
+        }
+        [HttpDelete]
+        public void ExcluirUsuario(int id)
+        {
+            _usuarioRepository.DeletarUsuario(id);
+        }
+    }
+}
---
+++ APIAlura/Interfaces/IUsuarioRepository.cs
@@ -0,0 +1,18 @@
+﻿using APIAlura.Entidade;
+
+namespace APIAlura.Interfaces
+{
+    public interface IUsuarioRepository
+    {
+        IList<Usuario> ObterTodosUsuarios();
+
+        Usuario ObterUsuarioPorId(int id);
+
+        void CadastrarUsuario(Usuario usuario);
+
+        void AlterarUsuario(Usuario usuario);
+
+        void DeletarUsuario(int id);
+
+    }
+}
